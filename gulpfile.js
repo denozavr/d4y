3b830602c36e1b7bc00e76d29c020a5c5039ce28
Den <denizdeniz90@gmail.com>
2019-01-14 12:21:03 +0300

    add plugins for gulp and add minification and prefix in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+var gulp = require('gulp');
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano'),
+  rename = require('gulp-rename'),
+  sourcemaps = require('gulp-sourcemaps');
 
 
 var paths = {
   styles: {
     // styles/**/*.scss tells GULP to check all folders for any scss file
-    src: "styles/**/*.scss",
+    src: 'styles/**/*.scss',
     // Compiled files in specified folder
-    dest: "dist"
+    finalFile: 'styles/styles.css',
+    destDev: 'styles',
+    dest: 'dist'
   }
 
 };
@@ -17,18 +24,35 @@ function style() {
   // .scss files are stored in the styles folder
   return gulp
     .src(paths.styles.src)
+    // Initialize sourcemaps before compilation starts
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .on("error", sass.logError)
+    .on('error', sass.logError)
+    // Use postcss with autoprefixer and compress the compiled file using cssnano
+    // .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(postcss([ autoprefixer({browsers: ['last 2 versions']}) ]))
+    // Now add/write the sourcemaps
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.destDev));
+}
+
+function styleMin() {
+  return gulp
+    .src(paths.styles.finalFile)
+    .pipe(postcss([cssnano()]))
+    .pipe(rename('styles.min.css'))
     .pipe(gulp.dest(paths.styles.dest));
 }
 
 function watch() {
   // run style task before implement gulp.watch
   style();
+  styleMin();
 
   // gulp.watch takes in the location of the files to watch for changes
   // and the name of the function we want to run on change
   gulp.watch(paths.styles.src, style);
+  gulp.watch(paths.styles.finalFile, styleMin);
 }
 
 // Don't forget to expose the task!
@@ -38,3 +62,4 @@ exports.watch = watch
 // This allows you to run it from the commandline using
 // $ gulp style
 exports.style = style;
+exports.min = styleMin;
