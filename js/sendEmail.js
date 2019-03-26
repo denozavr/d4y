4ff2c8a0f7a7ea353b044a3cbaff4720c9f39c3f
Den <denizdeniz90@gmail.com>
2019-03-26 16:11:46 +0300

    Autofocus invalid inputs when use contact form

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
       sendButton.disabled = false; //("disabled");
     }
 
-    var validForm = isValid(nameInput) && (isValid(messageInput) && isEmail(emailInput)) && policyCheckboxInput.checked;
+    var validForm = isValid(nameInput) & (isEmail(emailInput) && isValid(messageInput)) && policyCheckboxInput.checked;
 
     // disable send for 60 seconds if form is valid
     if (validForm) {
@@ -89,20 +89,42 @@ document.addEventListener('DOMContentLoaded', function () {
         );
 
     } else {
-      Swal.fire("Attention!", "You didn't fill required field(s) to send the message!", "warning");
+      Swal.fire("Attention!", "You didn't fill required field(s) to send the message!", "warning")
+      .then( function() {
+          setTimeout(function() {
+            if(invalidInputs.length > 0) invalidInputs[0].focus();
+          }, 750);
+        }
+        );
     }
   }
 
+    function deleteFromInvalidInputs(isValid, input) {
+      if (isValid && invalidInputs.indexOf(input) >= 0) {
+        invalidInputs.splice( invalidInputs.indexOf(input), 1 );
+      }
+    }
+
   function isValid(input) {
     var isValid = input.value.trim().length > 0;
+    if (!isValid && invalidInputs.indexOf(input) < 0) {
+      invalidInputs.push(input);
+    }
+    deleteFromInvalidInputs(isValid, input);
     return isValid;
   }
 
-  function isEmail(emailV) {
-    if (emailV != null && emailV != undefined && emailV.value.length > 0) {
+  function isEmail(emailInput) {
+    if (emailInput != null && emailInput != undefined && emailInput.value.length > 0) {
       var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-      return pattern.test(emailV.value);
+
+      deleteFromInvalidInputs(pattern.test(emailInput.value), emailInput);
+
+      return pattern.test(emailInput.value);
     } else {
+      if (invalidInputs.indexOf(emailInput) < 0) {
+        invalidInputs.push(emailInput);
+      }
       return false;
     }
   }
